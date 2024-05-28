1089a0684c5d224401396e30eeadd3e1389a2d9a
Mohammed Musharaf <mohammedm@zuper.local>
2024-05-28 08:12:56 -0600

    [FEAT] added LOGIN PAGE

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen w-screen  justify-between">
+      <div className="border-r flex-1 hidden md:block"></div>
+      <div className="flex-1 m-auto flex items-center justify-center px-6">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default AuthLayout;
---
+++ src/lib/types.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+export const LoginFormSchema = z.object({
+  email: z.string().describe("Email").email({ message: "Invalid Email" }),
+  password: z.string().describe("Password").min(1, "Password is required"),
+});
